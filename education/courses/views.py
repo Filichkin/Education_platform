a9c0952ecfc597de5dc722e66467000eea633756
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-09 20:32:47 +0300

    add mixins.py

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic.edit. import (
+    CreateView,
+    DeleteView,
+    UpdateView
+)
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .mixins import OwnerCourseMixin, OwnerCourseEditMixin
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/mange/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/mange/course/delete.html'
+
