743ad55b478d72107cd42e90c8a3a732ee33a72b
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-11 21:09:55 +0300

    add ContentCreateUpdateView

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import (
@@ -9,7 +11,43 @@ from django.views.generic.list import ListView
 
 from .forms import ModuleFormSet
 from .mixins import OwnerCourseMixin, OwnerCourseEditMixin
-from .models import Course
+from .models import Content, Course, Module
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(
+                app_label='courses', model_name=model_name
+            )
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(
+            model,
+            exclude=['owner', 'order', 'created', 'updated']
+        )
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(
+            Module,
+            id=module_id,
+            course__owner=request.user
+        )
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(
+                self.model,
+                id=id,
+                owner=request.user
+            )
+        return super().dispatch(self, request, module_id, model_name, id)
 
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
