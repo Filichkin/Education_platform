e5776a4ac011c8f78d96637100c58272aad35c10
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-18 21:42:22 +0300

    add render to ItemBase model

--- education/courses/models.py
+++ education/courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.template.loader import render_to_string
 
 from .constants import MAX_LENGTH
 from .fields import OrderField
@@ -144,6 +145,12 @@ class ItemBase(models.Model):
     updated = models.DateTimeField(
         auto_now=True,
         verbose_name='Дата и время обновления'
+        )
+
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self}
         )
 
     class Meta:
