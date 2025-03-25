8564c67b647acac6dd313be28a8a9629e1da3915
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-25 17:36:29 +0300

    add CourseViewSet

--- education/courses/api/views.py
+++ education/courses/api/views.py
@@ -1,9 +1,16 @@
 from django.db.models import Count
 from rest_framework import generics
+from rest_framework import viewsets
 
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
 
 
 class SubjectListView(generics.ListAPIView):
