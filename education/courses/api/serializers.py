bd324c9eef6e71b517ba68446e48c386b8197c51
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-26 10:58:05 +0300

    add CourseWithContentsSerializer

--- education/courses/api/serializers.py
+++ education/courses/api/serializers.py
@@ -1,13 +1,51 @@
 from django.db.models import Count
 from rest_framework import serializers
 
-from courses.models import Course, Module, Subject
+from courses.models import Content, Course, Module, Subject
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
 
 
 class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Module
         fields = ['order', 'title', 'description']
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.Serializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'desctiption', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.Serializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules'
+        ]
 
 
 class CourseSerializer(serializers.ModelSerializer):
